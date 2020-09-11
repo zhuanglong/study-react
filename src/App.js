556c642910b680e04c29a6fe79df84dd4a6ca571
zhuanglong <>
2020-09-11 18:10:12 +0800

    离开页面提示

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Route, Link, Prompt, withRouter } from 'react-router-dom';
 
 const Home = () => (
   <div>Home</div>
@@ -9,31 +9,31 @@ const About = () => (
   <div>About</div>
 );
 
-const CustomLink = ({ label, to, activeOnlyWhenExact }) => (
-  <Route
-    path={to}
-    exact={activeOnlyWhenExact}
-    children={({ match }) => (
-      <div>
-        {match ? '>' : ''}
-        <Link to={to}>{label}</Link>
-      </div>
-    )}
-  />
-);
+const MyPrompt = withRouter(({ location }) => {
+  return (
+    <Prompt
+      when={location.pathname === '/about'}
+      message={(dd) => {
+        const bool = window.confirm('您确定要离开该页面吗?');
+        return bool;
+      }}
+    />
+  );
+})
 
 function App() {
   return (
     <Router>
       <div>
         <ul>
-          <li><CustomLink to="/" label="Home" activeOnlyWhenExact>Home</CustomLink></li>
-          <li><CustomLink to="/about" label="About">About</CustomLink></li>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
         </ul>
         <hr />
         <Route exact path="/" component={Home} />
         <Route path="/about" component={About} />
       </div>
+      <MyPrompt />
     </Router>
   );
 }
