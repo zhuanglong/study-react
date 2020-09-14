fe4bb9c4839ed1f701b18844afd96cdb0d3c07ef
zhuanglong <>
2020-09-14 14:33:17 +0800

    404

--- src/App.js
+++ src/App.js
@@ -1,63 +1,33 @@
 import React from 'react';
-import { HashRouter as Router, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Route, Link, Switch } from 'react-router-dom';
 
-const routes = [
-  {
-    title: 'Home',
-    path: '/',
-    exact: true,
-    component: () => <div>Home</div>,
-  },
-  {
-    title: 'About',
-    path: '/about',
-    component: () => <div>About</div>,
-  },
-  {
-    title: 'Setting',
-    path: '/setting',
-    component: () => <div>Setting</div>,
-  }
-];
+const Home = () => (
+  <div>Home</div>
+);
+
+const About = () => (
+  <div>About</div>
+);
+
+const NoMatch = () => (
+  <div>404</div>
+);
 
 function App() {
   return (
     <Router>
-      <div
-        style={{
-          display: 'flex'
-        }}
-      >
-        <div
-          style={{
-            width: '60px',
-            marginRight: '10px',
-            padding: '20px',
-            backgroundColor: '#ccc'
-          }}
-        >
-          {routes.map((item) =>
-            <Route
-              exact={item.exact}
-              path={item.path}
-              children={({ match }) =>
-                <div>
-                  {match ? '>' : ''}
-                  <Link to={item.path}>{item.title}</Link>
-                </div>
-              }
-            />
-          )}
-        </div>
-        <div
-          style={{
-            flex: 1
-          }}
-        >
-          {routes.map((item) =>
-            <Route exact={item.exact} path={item.path} component={item.component} />
-          )}
-        </div>
+      <div>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/setting">Setting</Link></li>
+        </ul>
+        <hr />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="*" component={NoMatch} />
+        </Switch>
       </div>
     </Router>
   );
